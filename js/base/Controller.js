d1ea44574834d3a0a5da8a5743eec6cd68abf178
jiangdahe <jiangdaheda2@sina.com>
2019-09-24 15:59:49 +0800

    添加整个MVC模型，并加入类的概念

---
+++ js/base/Controller.js
@@ -0,0 +1,39 @@
+window.Controller = function (options) {
+    var init = options.init        //这个init是变量    //B
+    // console.log('options.init')
+    // console.log(options.init)
+    let object = {
+        view: null,
+        model: null,
+        init: function (view, model) {  //这个init是属性    //A
+            this.view = view
+            this.model = model
+            this.model.init()
+            init.call(this, view, model) //这里边的this是object，这里有好几个弯 //C
+            //这里的this（C）是init（A）调用的时候传的，而init（A）是controller.init（）调用的，所以这个this就是controller，controller又是Controller返回的，
+            //Controller返回的是object，因此，这个this就是object
+            //此处调用的init就是options.init，而options是传进来的，在message.js的controller里的init属性里，但是它的几个属性在object里都没有
+            //而options里都有，因此将除了init之外的属性（init在刚开始都提取了）都复制到object里就行了
+            //之所以这样是因为this.messageList = view.querySelector('#messageList')
+            // this.form = view.querySelector('form')
+            // this.loadMessage()
+            //this.bindEvents.call(this)
+            //这几个this最终会传入object
+            this.bindEvents.call(this)
+        },
+    }
+    console.log('object')
+    console.log(object)
+    debugger
+    console.log('options')
+    console.log(options)
+    debugger
+    for (let key in options) {
+        if (key !== 'init') {
+            object[key] = options[key]
+        }
+    }
+    console.log('新的 object')
+    console.log(object)
+    return object
+}
--- js/message.js
+++ js/message.js
@@ -1,48 +1,11 @@
 !function () {
     var view = View('section.message')
-    var model = {
-        //获取数据
-        init: function () { //初始化也是操作数据，所以将它移到model里
-            var APP_ID = 'zRB0Ais0Bx9MFp6GAGMVsRyd-gzGzoHsz';
-            var APP_KEY = 'ptVyDtTayVK28bHshIEUKkRS';
-            AV.init({
-                appId: APP_ID,
-                appKey: APP_KEY
-            });
-            console.log('运行到这里没有错误')
-        },
-        fetch: function () {
-            var query = new AV.Query('Message');
-            return query.find() //返回的是Promise对象
-        },
-        //创建数据
-        save: function (name, content) {
-            var Message = AV.Object.extend('Message');
-            var message = new Message();
-            // message.set({   //将单个内容变成多个用对象
-            //     'name': name,  //这个set会将这个对象的两个值并为一个键值对
-            //     'content': content  //即  name:content  
-            // });
-            // console.log(message.save(name, content))
-            return message.save({   //将单个内容变成多个用对象
-                'name': name,
-                'content': content
-            })//返回的是Promise对象       
-        }
-    }
-    var controller = {
-        view: null,
-        model: null,
-        messageList: null,
+    var model = Model({ resourceName: 'Message' })
+    var controller = Controller({
         init: function (view, model) {
-            this.view = view
-            this.model = model
-            console.log(this)
             this.messageList = view.querySelector('#messageList')
             this.form = view.querySelector('form')
-            this.model.init()
             this.loadMessage()
-            this.bindEvents()
         },
         loadMessage: function () {
             this.model.fetch()
@@ -66,30 +29,70 @@
             let content = myForm.querySelector('input[name=content]').value //获取内容
             let name = myForm.querySelector('input[name=name]').value//获取名字
 
-            this.model.save(name, content)
+            this.model.save({
+                'name': name,
+                'content': content
+            })
                 .then(function (Object) {
                     let li = document.createElement('li')
                     li.innerText = `${Object.attributes.name}:${Object.attributes.content}` //这里因为没有像上面的array提取
                     messageList.appendChild(li)   //这4句是为了直接在页面显示输入内容
                     myForm.querySelector('input[name=content]').value = ''//清空密码输入框的内容
-                    console.log(Object)
+                    console.log('我走到这里了')
                 })
         }
-    }
+    })
+
+    // var controller = {
+    //     view: null,
+    //     model: null,
+    //     messageList: null,
+    //     init: function (view, model) {
+    //         this.view = view
+    //         this.model = model
+    //         console.log(this)
+    //         messageList = view.querySelector('#messageList')
+    //         this.form = view.querySelector('form')
+    //         this.model.init()
+    //         this.loadMessage()
+    //         this.bindEvents()
+    //     },
+    //     loadMessage: function () {
+    //         this.model.fetch()
+    //             .then((message) => {// 获取需要更新的 message
+    //                 let array = message.map((item) => item.attributes)//用map函数清洗数据
+    //                 array.forEach((item) => {
+    //                     let li = document.createElement('li')
+    //                     li.innerText = `${item.name}:${item.content}`
+    //                     this.messageList.appendChild(li)
+    //                 })
+    //             })
+    //     },
+    //     bindEvents: function () {
+    //         this.form.addEventListener('submit', (e) => {
+    //             e.preventDefault()
+    //             this.saveMessage()
+    //         })
+    //     },
+    //     saveMessage: function () {
+    //         let myForm = this.form
+    //         let content = myForm.querySelector('input[name=content]').value //获取内容
+    //         let name = myForm.querySelector('input[name=name]').value//获取名字
+
+    //         this.model.save({
+    //             'name': name,
+    //             'content': content
+    //         })
+    //             .then(function (Object) {
+    //                 let li = document.createElement('li')
+    //                 li.innerText = `${Object.attributes.name}:${Object.attributes.content}` //这里因为没有像上面的array提取
+    //                 messageList.appendChild(li)   //这4句是为了直接在页面显示输入内容
+    //                 myForm.querySelector('input[name=content]').value = ''//清空密码输入框的内容
+    //                 console.log(Object)
+    //             })
+    //     }
+    // }
 
     controller.init(view, model) //和view一样，model也要传进来
 }.call()
 
-/*
-//创建TestObject表
-var TestObject = AV.Object.extend('TestObject');
-//在表中创建一行数据
-var testObject = new TestObject();
-
-
-testObject.set('words', 'Hello world!');
-//如果保存成功则运行console.log
-testObject.save().then(function (testObject) {
-  console.log('保存成功。')
-})
-*/
